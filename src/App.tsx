8ea7c0936e30a23fe6daa515818a20a552f29f5b
Samantha Shulman <samantha_shulman@brown.edu>
2023-07-09 22:10:48 -0400

    initial commit 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>Hello all</h1>
     </div>
   );
 }
